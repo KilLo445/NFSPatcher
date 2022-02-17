29fc816b56e03f332652293e18a9079a0b27c42e
KilLo445 <20kaedyn05@gmail.com>
2022-02-16 22:07:22 -0500

    Add files via upload

---
+++ patcher/underground2/NoCD/nocd.bat
@@ -0,0 +1,5 @@
+@echo off
+title Need for Speed: Underground No CD Patch
+del SPEED2.exe >nul
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/KilLo445/NFSPatcher/raw/main/patcher/underground2/NoCD/SPEED2.EXE', 'SPEED2.EXE')"
+exit
