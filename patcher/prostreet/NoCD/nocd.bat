2beeee70f7cfbeda1dac56b4867a448bd8ca0d90
KilLo445 <20kaedyn05@gmail.com>
2022-05-17 12:11:06 -0400

    Delete nocd.bat

--- patcher/prostreet/NoCD/nocd.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Need for Speed: ProStreet No CD Patch
-del nfs.exe >nul
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/KilLo445/NFSPatcher/raw/main/patcher/prostreet/NoCD/nfs.exe', 'nfs.exe')"
-exit
