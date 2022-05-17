8480368f526bb1088516b9e960d627e2b913a5a5
KilLo445 <20kaedyn05@gmail.com>
2022-05-17 12:10:34 -0400

    Delete nocd.bat

--- patcher/carbon/NoCD/nocd.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Need for Speed: Carbon Collector's Edition No CD Patch
-del NFSC.exe >nul
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/KilLo445/NFSPatcher/raw/main/patcher/carbon/NoCD/NFSC.exe', 'NFSC.exe')"
-exit
